7467d37ae678474ddf0ff14b71128dd9f3bb5d78
csmaterial <csmaterial@users.noreply.github.com>
2017-11-22 11:47:32 +0800

    Create user_change_num.py

---
+++ user_change_num.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 11:26:12 2017
+
+@author: czy
+"""
+
+import re
+import numpy as np
+import pandas as pd
+address = '/home/czy/beerdata.csv'
+
+ee = pd.read_csv(address)
+
+def makenamelist(ee,col):
+        
+    user = ee[col]
+    l = []
+    
+    for i in range(len(ee)):
+        te = user[i]
+        #print(te)
+        if te not in l:
+            l.append(te)
+        
+    for i in range(len(ee)):
+        te = user[i]
+        num = l.index(te)
+        user[i] = num
+    return user
+
+#user = makenamelist(ee,'user')
+item = makenamelist(ee,'item')
+
+newaddress = '/home/czy/user_beerdata.csv'
+ee.to_csv(newaddress)  
+
