691b4fb9e9e9e5d48c7d4ee2584b2978982a89e6
csmaterial <csmaterial@users.noreply.github.com>
2017-11-28 09:39:23 +0800

    Update check_matrix_set.py

--- check_matrix_set.py
+++ check_matrix_set.py
@@ -1,1 +1,15 @@
 
+pannum = 0
+us = 1
+for i in range(len(newdata)):
+    #
+    
+    
+    if newdata[i,0] ==  us:
+        if pannum == 0:
+            sa = newdata[i:i+1,:]
+            pannum+=1
+        else:
+            sa1 = newdata[i:i+1,:]
+            sa=np.concatenate((sa,sa1),axis=0)
+    else:
