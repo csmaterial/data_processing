e247b8631693289bcee325fc3a00f084e4acda67
csmaterial <csmaterial@users.noreply.github.com>
2018-01-05 22:20:50 +0800

    Create kaggledemo.py

---
+++ kaggledemo.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+data = 
+target = 
+
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=6)
+
